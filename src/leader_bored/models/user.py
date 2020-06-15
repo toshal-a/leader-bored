4ba3fde84659cf77f1a1f49c1064cf5823d07480
walragatver <toshalagrawal38@gmail.com>
2020-06-15 14:59:44 +0530

    Change default value of is_active

--- src/leader_bored/models/user.py
+++ src/leader_bored/models/user.py
@@ -11,7 +11,7 @@ class Users(Base):
     handle = Column(String,unique=True,nullable=False)
     rank = Column(Integer,default=None)
     overall_score = Column(Integer,default=0,nullable=False)
-    is_active = Column(Boolean(), default=True,nullable=False)
+    is_active = Column(Boolean(), default=False,nullable=False)
     is_superuser = Column(Boolean(), default=False,nullable=False)
     created_at = Column(DateTime,server_default=func.now())
     updated_at = Column(DateTime,server_default=func.now(),onupdate=func.now())
