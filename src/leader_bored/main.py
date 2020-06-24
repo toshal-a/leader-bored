ded8a0e54726a5ed8b44b0ee6f3a5d953f268cb7
TheJina <jainyakshittwo@live.com>
2020-06-24 15:30:20 +0530

    Ad root_path in main app config

--- src/leader_bored/main.py
+++ src/leader_bored/main.py
@@ -11,7 +11,7 @@ except ImportError:  # pragma: no cover
 logger = logging.getLogger(__name__)
 
 def get_app():
-    app = FastAPI(title="LeaderBored API")
+    app = FastAPI(title="LeaderBored API",root_path="/api")
     load_modules(app)
     return app
 
